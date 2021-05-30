dc1a5608e70df02c5013b0438996a953211e76c2
dvirberlo <dvir.berlowitz@hotmail.com>
2021-05-30 19:45:43 +0300

    protected -> getData

--- controllers/protectedController.js
+++ controllers/protectedController.js
@@ -1,24 +1,30 @@
-// const User = require('../models/user')
+const User = require('../models/user')
 const AuthToken = require('../models/authToken')
 const createError = require('http-errors')
-// const client = require('../lib/client')
+const client = require('../lib/client')
 
 /** Protected Controller
- * /protected
- *   /login/:username/:password/:remeber -> id
- *   /:username
- *     / -> data
- *     /move/:des -> ?err
- *     /quit -> ?err
- *     /enter/:id -> ?err
- *     /battle/:id -> ?err
- *     /mission -> available mission
+ * /protected (authCheck)
+ *   / -> data
+ *   /move/:des -> ?err
+ *   /quit -> ?err
+ *   /enter/:id -> ?err
+ *   /battle/:id -> ?err
+ *   /mission -> available mission
  */
 exports.authCheck = (req, res, next) => {
+  req.UserId = req.cookies.UserId
   req.auth = false
-  AuthToken.find({ user: req.cookies.UserId }, (err, docs) => {
+  AuthToken.find({ user: req.UserId }, (err, docs) => {
     if (err) return next(createError(err))
     for (const doc of docs) req.auth = req.auth || req.cookies.AuthToken === doc.token
-    next()
+    if (req.auth) next()
+    else next(createError.Unauthorized())
+  })
+}
+exports.getData = (req, res, next) => {
+  User.findById(req.UserId, { _id: 0, __v: 0, password: 0 }, (err, doc) => {
+    if (err) return next(createError(err))
+    client.send(res, doc)
   })
 }
--- routes/protected.js
+++ routes/protected.js
@@ -6,4 +6,6 @@ const protectedController = require('../controllers/protectedController')
 /* GET protected listing. */
 router.use(protectedController.authCheck)
 
+router.get('/', protectedController.getData)
+
 module.exports = router
