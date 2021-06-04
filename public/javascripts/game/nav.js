1fa2de2c5c319ae5c57e52b6eb0fe8871d05f9e9
dvirberlo <dvir.berlowitz@hotmail.com>
2021-06-04 13:25:13 +0300

    nav update

--- public/javascripts/game/nav.js
+++ public/javascripts/game/nav.js
@@ -2,5 +2,8 @@
 
 window.nav = { update }
 function update (user) {
-  // TODO
+  const $details = $('#navDetails')
+  for (const res in user.resources) $details.find('#' + res).text(user.resources[res])
+  $details.find('#navUsername').text(user.username)
+  $details.find('#navXp').text(user.xp)
 }
